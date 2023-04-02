fa96b04a110b050134e365e4844e32f028f0d538
rivillasm <rivillasm@gmail.com>
2023-04-01 22:00:39 -0600

    updates

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 import logging
 
 def error_message_detail(error, error_detail:sys):
@@ -18,9 +19,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message    
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Division by zero")
-        raise CustomException(e,sys)
