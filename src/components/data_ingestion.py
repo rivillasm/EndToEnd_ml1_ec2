48619aa310263cd4df1ee779f7e7820b3a75571b
rivillasm <rivillasm@gmail.com>
2023-04-03 21:24:03 -0600

    data transformation update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -41,8 +42,14 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
+    # initiates ingestion and provides the paths for the train/test data
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()    
+    
+    # initiates data transformation and provides the transformed datasets 
+    # and saves the preprocessor
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
 
     
